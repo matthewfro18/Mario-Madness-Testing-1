5aa4f366697587879b4e967df773665d52ea3ea2
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-04 20:21:43 -0400

    Update and rename unix.sh to setup-unix.sh

---
+++ setup/setup-unix.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+#
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download/version/4.2.5/
+haxelib install lime 7.9.0
+haxelib install openfl
+haxelib install flixel 5.2.1
+haxelib install flixel-addons 3.0.2
+haxelib install flixel-ui
+haxelib install flixel-tools
+haxelib install SScript
+haxelib install hxCodec 2.6.1
+haxelib install tjson
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
